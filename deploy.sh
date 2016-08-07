8190e4c06777615fc41bc52acccb8958a8f5fe79
Lee Xiang Wei <xwlee2009@gmail.com>
2016-08-07 14:34:41 +0800

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+docker build -t xwlee/sample-node .
+docker push xwlee/sample-node
+
+ssh deploy@128.199.77.198 << EOF
+docker pull xwlee/sample-node:latest
+docker stop web || true
+docker rm web || true
+docker rmi xwlee/sample-node:current || true
+docker tag xwlee/sample-node:latest xwlee/sample-node:current
+docker run -d --net app --restart always --name web -p 3000:3000 xwlee/sample-node:current
+EOF
